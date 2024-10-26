e0f4e439da03946a1d52b7bbeccd1d90015e1d53
hycienti <h.igweze@alustudent.com>
2024-10-26 06:28:55 -0700

    fixs

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
+    result = 1.0
     if b < 0:
         a = 1 / a
         b = -b
     for _ in range(b):
         result *= a
-    return round(result, 10)
+    return result
