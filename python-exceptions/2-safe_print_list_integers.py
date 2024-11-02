6df2c8810d25145e5843066633c977465a1b7713
hycienti <h.igweze@alustudent.com>
2024-11-02 15:26:16 -0700

    fix: white space error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0  # Keep track of the actual number of integers printed
-    
     for i in range(x):
         try:
             # Attempt to format and print the item as an integer
@@ -10,6 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             # If the item is not an integer, skip it silently
             continue
-    
     print()  # Print a newline after all integers
-    return count  # Return the number of integers printed
+    return count  # Return the number of integers printeid
