f3bc431366cd2c0a4e6fa1dc256a631af8e8545d
hycienti <h.igweze@alustudent.com>
2024-11-04 23:23:46 -0800

    perf: tired

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -79,4 +79,6 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        return "\n".join(
+            ["#" * self.__width for _ in range(self.__height)]
+        )
