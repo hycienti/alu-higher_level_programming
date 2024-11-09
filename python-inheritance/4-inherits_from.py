502ae85bce317c43112bb1e5ca801ed4ebf3ac5b
hycienti <h.igweze@alustudent.com>
2024-11-09 10:41:50 -0800

    fix: pep8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,7 +8,8 @@ a class that inherited (directly or indirectly) from a specified class.
 def inherits_from(obj, a_class):
     """
     Returns True if the object is an instance of a class that inherited
-    (directly or indirectly) from the specified class; otherwise, returns False.
+    (directly or indirectly) from the specified class; otherwise, returns
+    False.
 
     Args:
         obj: The object to check.
