3fa7b4c904f8b7e2a91f9cd02625ccb313877492
hycienti <h.igweze@alustudent.com>
2024-11-09 10:46:41 -0800

    feat: lass BaseGeometry (based on 5-base_geometry.py).

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry with an unimplemented area method.
+"""
+
+
+class BaseGeometry:
+    """
+    A class that represents geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception to indicate that the area method is not implemented.
+        """
+        raise Exception("area() is not implemented")
