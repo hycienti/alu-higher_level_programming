5275b46b3120f4e52467f2d9f2a0c7947001608f
hycienti <h.igweze@alustudent.com>
2024-11-14 10:53:08 -0800

    fix: pep8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This module provides a function to read and print the content of a UTF-8 encoded 
-text file.
+This module provides a function to read and print the content of a UTF-8
+encoded text file.
 
-The `read_file` function takes a filename as an argument, opens the file in read 
-mode with UTF-8 encoding, and prints its contents to stdout. This function uses 
-Python's `with` statement to ensure safe file handling.
+The `read_file` function takes a filename as an argument, opens the file in
+read mode with UTF-8 encoding, and prints its contents to stdout. This
+function uses Python's `with` statement to ensure safe file handling.
 
 Functions:
-    read_file(filename=""): Reads a UTF-8 encoded text file and prints its content 
-    to stdout.
+    read_file(filename=""): Reads a UTF-8 encoded text file and prints its
+    content to stdout.
 """
 
 def read_file(filename=""):
@@ -17,7 +17,8 @@ def read_file(filename=""):
     Reads a UTF-8 encoded text file and prints its content to stdout.
     
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read. Defaults to an empty
+        string.
         
     Returns:
         None
