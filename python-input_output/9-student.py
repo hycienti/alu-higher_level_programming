686d79792a6ed360eddf4469a4b171500f85b862
hycienti <h.igweze@alustudent.com>
2024-11-16 07:43:51 -0800

    feat: Public method def to_json(self): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py)

---
+++ python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with attributes
+and a method to retrieve its dictionary representation.
+"""
+
+
+class Student:
+    """
+    Defines a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Returns:
+            dict: The dictionary representation of the instance.
+        """
+        return self.__dict__
