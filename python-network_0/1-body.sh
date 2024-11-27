8cf258fe45180fbe866912ac1a36c52280d8458f
hycienti <h.igweze@alustudent.com>
2024-11-27 19:27:10 +0000

    fix: retru

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body if the response is 200
+# Sends a GET request to a URL and displays the body of the response for a 200 status code
 curl -s -o /dev/null -w '%{http_code}' "$1" | grep -q "^200$" && curl -s "$1"
