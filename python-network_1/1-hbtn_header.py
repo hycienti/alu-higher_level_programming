22a290ce6e89679c8d11c67da81403b770ff18dc
hycienti <h.igweze@alustudent.com>
2024-11-27 20:25:05 +0000

    chmod +x 1-hbtn_header.py

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module sends a request to a given URL and displays the X-Request-Id header value.
+It demonstrates header retrieval using urllib and sys packages.
+"""
+import urllib.request
+import sys
+
+
+def get_x_request_id(url):
+    """
+    Fetch and print the X-Request-Id header from the given URL.
+    
+    Args:
+        url (str): The URL to send the request to
+    """
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)
+
+
+if __name__ == "__main__":
+    get_x_request_id(sys.argv[1])
