ad9b4476d317e137054914740ef66ca7e9aa8409
hycienti <h.igweze@alustudent.com>
2024-11-27 20:15:18 +0000

    This script fetches https://alu-intranet.hbtn.io/status using urllib and displays the body of the response with specific formatting.

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alu-intranet.hbtn.io/status using urllib
+and displays the body of the response with specific formatting.
+"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    url = 'https://alu-intranet.hbtn.io/status'
+    
+    with request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
