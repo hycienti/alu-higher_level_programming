028d63560c362571b74c17a28072148c643637d4
hycienti <h.igweze@alustudent.com>
2024-11-27 19:40:39 +0000

    fixing

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header and displays the body of the response 98
+# Sends a GET request to a URL with a custom header and displays the response body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
