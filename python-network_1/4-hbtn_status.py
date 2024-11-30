e9be9dda4f7a2967c89a50fa757f8ce748ed8b0c
hycienti <h.igweze@alustudent.com>
2024-11-30 04:09:37 +0000

    fix: i am tired by now

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-This script fetches https://alu-intranet.hbtn.io/status using the
-requests package and displays the response body in a specific format.
+This script makes a request and displays a
+response using the requests package.
 """
 
 import requests
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
-
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
